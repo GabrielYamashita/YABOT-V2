f68f4cad23857dafa903f7f1e9e57b0836fbf26b
Yama <gabriel.yamashita01@gmail.com>
2024-01-07 02:04:59 -0300

    fix ports

--- app.py
+++ app.py
@@ -118,4 +118,6 @@ if __name__ == "__main__":
     scheduler.start()
 
     # Start Flask app
-    app.run()
+    port = os.getenv("PORT", 5000)
+    app.run(debug=False, host="0.0.0.0", port=port)
+
