c4d6fcc636ab84ac8778dae6cdd127cd95066e9d
Yama <gabriel.yamashita01@gmail.com>
2024-01-20 05:02:09 -0300

    qr code gen

--- utils/webhook_command.py
+++ utils/webhook_command.py
@@ -1,11 +1,17 @@
 
 def process_command(command, incoming_msg):
     # Implement command processing logic
-    if 'gen qr' in command.lower():
+    imgGen = False
 
-        return f'{command}'
+    if 'qr' in command.lower():
+        link = command.split(' ')[1:]
 
+        resp = 'QR Code Generated'
+        imgGen = f'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%3C{link}%3E'
 
-    return f"Command processed: '{command}'"
+    else:
+        resp = 'Command not Found!'
+
+    return resp, imgGen
 
 
