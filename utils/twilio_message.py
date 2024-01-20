cfe029d430f04a826680d619ddab5f7d6219198e
Yama <gabriel.yamashita01@gmail.com>
2024-01-20 04:17:14 -0300

    files organization

--- utils/twilio_message.py
+++ utils/twilio_message.py
@@ -21,7 +21,7 @@ def respond(incoming_msg):
         command = " ".join(msgBody.split(" ")[1:])
 
         # Handle the command and generate a response
-        # response_message = process_command(command, incoming_msg)
+        response_message = process_command(command, incoming_msg)
         response_message = 'sla'
         msg.body(response_message)
             
@@ -72,3 +72,29 @@ def send_message(message):
 #         ('ApiVersion', '2010-04-01')
 #     ]
 # )
+
+from werkzeug.datastructures import ImmutableMultiDict
+
+# Creating an empty ImmutableMultiDict
+# immutable_multi_dict = ImmutableMultiDict()
+
+# Creating an ImmutableMultiDict with some initial data
+data = [
+    ('SmsMessageSid', 'SMc7e34b5928ea30ede3380c63082a3b69'),
+    ('NumMedia', '0'),
+    ('ProfileName', 'Gabriel Yamashita'),
+    ('SmsSid', 'SMc7e34b5928ea30ede3380c63082a3b69'),
+    ('WaId', '5511991982436'),
+    ('SmsStatus', 'received'),
+    ('Body', '!command show log'),
+    ('To', 'whatsapp:+14155238886'),
+    ('NumSegments', '1'),
+    ('ReferralNumMedia', '0'),
+    ('MessageSid', 'SMc7e34b5928ea30ede3380c63082a3b69'),
+    ('AccountSid', 'ACc906b1cb84d639c680889d5ab72f36d1'),
+    ('From', 'whatsapp:+5511991982436'),
+    ('ApiVersion', '2010-04-01')
+]
+immutable_multi_dict_with_data = ImmutableMultiDict(data)
+
+respond(immutable_multi_dict_with_data)
