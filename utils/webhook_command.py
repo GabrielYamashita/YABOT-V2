15f5a12a781049ad161c79d8942d3498e231ea26
Yama <gabriel.yamashita01@gmail.com>
2024-01-20 04:56:24 -0300

    qr code gen

--- utils/webhook_command.py
+++ utils/webhook_command.py
@@ -1,8 +1,9 @@
 
 def process_command(command, incoming_msg):
     # Implement command processing logic
-    if command == 'show log':
-        return f'Incoming Message:\n\n{incoming_msg}'
+    if 'gen qr' in command.lower():
+
+        return f'{command}'
 
 
     return f"Command processed: '{command}'"
